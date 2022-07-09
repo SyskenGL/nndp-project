bee48ae360b909a53c97f3cb5a2799e03417e8bd
SyskenGL <gianluca.larco@gmail.com>
2022-07-09 12:46:32 +0200

    Minor changes

--- nndp/core/models.py
+++ nndp/core/models.py
@@ -7,14 +7,14 @@ from nndp.core.layers import Layer
 class Sequential:
 
     def __init__(self, layers: list[Layer] = None):
-        if len(layers) > 1:
-            for h in range(1, len(layers)):
-                if layers[h].in_size != layers[h - 1].width:
-                    raise IncompatibleLayersError(
-                        f"in_size {layers[h].in_size} is not equal "
-                        f"to previous layer width {layers[h - 1].width}."
-                    )
         self.layers = layers if layers else []
+        if len(self.layers) > 1:
+            for h in range(1, len(self.layers)):
+                if self.layers[h].in_size != self.layers[h - 1].width:
+                    raise IncompatibleLayersError(
+                        f"in_size {self.layers[h].in_size} is not equal "
+                        f"to previous layer width {self.layers[h - 1].width}."
+                    )
 
     def add(self, layer: Layer):
         if len(self.layers) > 1:
