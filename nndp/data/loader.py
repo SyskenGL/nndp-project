9ce16882bbb0929528e32a53c33e752ddcd54a66
SyskenGL <gianluca.larco@gmail.com>
2022-08-22 21:02:20 +0200

    refactor minore

--- nndp/data/loader.py
+++ nndp/data/loader.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import os
+import mnist
 import numpy as np
-from mnist import MNIST
 from nndp.utils.collections import Dataset
 
 
@@ -27,14 +27,14 @@ class Loader:
         raise NotImplementedError
 
 
-class MNISTLoader(Loader):
+class MNIST(Loader):
 
     def __init__(self):
         super().__init__()
-        self._mnist_data = MNIST(os.path.join(os.path.dirname(__file__), "mnist"))
+        self._mnist_data = mnist.MNIST(os.path.join(os.path.dirname(__file__), "mnist"))
         data, labels = self._mnist_data.load_training()
         data = np.array(data).T
-        labels = MNISTLoader.encode(np.array(labels))
+        labels = MNIST.encode(np.array(labels))
         self._dataset = Dataset(data, labels)
 
     @staticmethod
