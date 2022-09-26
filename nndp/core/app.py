6a7ad7f4845edbbc3a34793bcbc73106459b2470
SyskenGL <gianluca.larco@gmail.com>
2022-09-26 11:32:50 +0200

    app update

--- nndp/core/app.py
+++ nndp/core/app.py
@@ -17,24 +17,37 @@ if __name__ == "__main__":
     loader = MNIST()
 
     # Suddivisione dataset in training set, validation test e test set
-    dataset = loader.scaled_dataset.random(instances=1000)
-    training_set, validation_set = dataset.split(0.25)
+    dataset = loader.scaled_dataset.random(instances=5000)
 
-    mlp = MLP(
-        [
-            ResilientDense(100, Activation.SIGMOID),
-            ResilientDense(10, Activation.IDENTITY),
-        ],
-        Loss.SOFTMAX_CROSS_ENTROPY
-    )
-    mlp.build(784)
-
-    stats = mlp.fit(
-        training_set,
-        validation_set,
-        epochs=300,
-        stats=[Metric.LOSS, Metric.ACCURACY, Metric.F1],
-        early_stops=[EarlyStop(Metric.LOSS, 0.8, greedy=True)],
-        eta_negative=0.25,
-        eta_positive=1.05
-    )
+    with open("k_fold.csv", mode="w") as file:
+        writer = csv.DictWriter(file, ["neurons", "eta_negative", "eta_positive", "accuracy"])
+        writer.writeheader()
+        for neurons in [85, 90, 95]:
+            for eta_negative in [0.05, 0.10, 0.15, 0.20, 0.25]:
+                for eta_positive in [1.05, 1.10, 1.15, 1.20, 1.25]:
+                    mlp = MLP(
+                        [
+                            ResilientDense(neurons, Activation.SIGMOID),
+                            ResilientDense(10, Activation.IDENTITY),
+                        ],
+                        Loss.SOFTMAX_CROSS_ENTROPY
+                    )
+                    mlp.build(784)
+                    results = mlp.cross_validate(
+                        dataset,
+                        5,
+                        metrics=[Metric.ACCURACY],
+                        epochs=50,
+                        eta_negative=eta_negative,
+                        eta_positive=eta_positive
+                    )
+                    print(
+                        f"neurons: {neurons} - eta negative: {eta_negative} - "
+                        f"eta_positive {eta_positive} - results: {results}"
+                    )
+                    writer.writerow({
+                        "neurons": neurons,
+                        "eta_negative": eta_negative,
+                        "eta_positive": eta_positive,
+                        "accuracy": f"avg: {results['accuracy'][0]} - std: {results['accuracy'][1]}"
+                    })
